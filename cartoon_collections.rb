8e5e78bef8befe7c79e5b87b949bb8a16d669bb7
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-04 04:04:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(array)
   array.each_with_index { |name, index| puts "#{index+1}. #{name}"}
 end
@@ -15,9 +17,10 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  binding.pry
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheese_types.include?(array)
     array.first
-  
+
   end
 end
