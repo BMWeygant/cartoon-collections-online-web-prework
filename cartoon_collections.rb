b559a07be462da1fe95375ca024e753f570f2e9a
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-04 04:22:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  binding.pry
+  #binding.pry
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |a|
     a.include?("cheese_types")
