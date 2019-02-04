b6d6b1fd57d79d4c0c60504887395d48867accdc
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-04 04:18:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ end
 def find_the_cheese(array)
   #binding.pry
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|a| a.include?("#{cheese_types")}
+  array.find {|a| a.include?("#{cheese_types}")}
 end
